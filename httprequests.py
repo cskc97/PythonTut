a36c18af0d1f3c444d0c6ad1937486b9526c1271
Santhoshkrishnachaitanya Chelikavada <everythingskc@gmail.com>
2017-05-14 12:32:56 +0530

    stuff

--- httprequests.py
+++ httprequests.py
@@ -2,8 +2,9 @@ import requests;
 import json;
 
 
-r = requests.get('https://api.github.com/user', auth=('usrname', 'pass'))
+r = requests.get('https://api.github.com/user', auth=('username', 'password'))
 print(r.json());
 
-jsonString = r.json();
+jsonString = r.text;
 data = json.loads(jsonString);
+print data['public_repos'];
