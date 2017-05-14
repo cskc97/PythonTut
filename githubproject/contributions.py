67958d49f33819aeb3d47c45c21d8c26c604193d
Santhoshkrishnachaitanya Chelikavada <everythingskc@gmail.com>
2017-05-14 23:00:13 +0530

    stuff

---
+++ githubproject/contributions.py
@@ -0,0 +1,17 @@
+import grequests
+import requests
+
+# Contributions are counted as follows -
+# Issues, pull requests, forks, commits
+
+#Getting commits as follows -
+# https://api.github.com/users/cskc97/repos - will give a list of all repos
+# for each repo name we will then have to call https://api.github.com/repos/cskc97/PythonTut/stats/commit_activity
+# where pythotut will be replaced by the repo name
+# the resulting array would span 365 days and have all the commits for each of those 365 days
+
+
+
+
+
+
