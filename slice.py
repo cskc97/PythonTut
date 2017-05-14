b9e7cc66bb806fa790d272c2268250f101c6a27b
Santhoshkrishnachaitanya Chelikavada <everythingskc@gmail.com>
2017-05-14 12:50:27 +0530

    Stuff

---
+++ slice.py
@@ -0,0 +1,18 @@
+list = [1,2,3,4,5,6,7,8,9,10];
+print(list);
+
+print(list[5]);
+
+#Now for the slice operator list[a:b] will print values for 
+#indices a through b-1. 
+
+print(list[2:5]);
+
+#You can also indicate steps. 
+print(list[2:5:2]);
+
+#make assignments 
+
+listTwo = list[5:10]
+
+print(listTwo);
