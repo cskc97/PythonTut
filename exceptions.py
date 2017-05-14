39b6bbef5a7d90100104f615389c86ddc4d05bd8
Santhoshkrishnachaitanya Chelikavada <everythingskc@gmail.com>
2017-05-14 19:02:58 +0530

    stuff

--- exceptions.py
+++ exceptions.py
@@ -7,10 +7,14 @@ except:
 try:
 	fh=open('nofilelikethis.txt')
 except IOError as e:
-	print(e, "This is an exception");
+	print(e, "This is an excepitgion");
 else:
 	#this will run if fh successfully opens nofilelikethis.txt
 	print("Opened successfully");
+
+
+# use raise keyword followed by YourExceptionName("message") to raise
+# exceptions 
 
 
 
--- functions.py
+++ functions.py
@@ -21,3 +21,15 @@ def isPrime(n):
 for n in range(1,20):
     isPrime(n)
 
+
+def passFunc():
+    pass
+# the above function does nothing. pass is a keyword that 
+# lets you create an empty function. 
+#None will let you create an optional argument
+def testFunc(*args):
+    # *args is a way of saying there is an optional list of arguments
+    print(args)
+    #args now becomes a tuple and is thus immutable. 
+
+testFunc(12,3,4)
