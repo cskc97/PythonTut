a770e05c8e657068819d8aa9f559e4709cc74435
Santhoshkrishnachaitanya Chelikavada <everythingskc@gmail.com>
2017-05-17 15:50:46 +0530

    stuff

--- tuts/classes.py
+++ tuts/classes.py
@@ -1,10 +1,22 @@
-class Duck:
+class Animal:
+	def __init__(self):
+		print("Animal Created")
+
+	def animal(self):
+		print("I am an animal")
+
+
+
+class Duck(Animal):
 	def __init__(self,duckName):
 		self.name = duckName
+
 	def quack(self):
 		print("Quack!")
 	def walk(self):
 		print("Duck Walking!")
+
+
 
 def main():
 	donald = Duck("Donald The Duck")
@@ -13,6 +25,8 @@ def main():
 	donald.walk()
 
 #NOTE: There are NO access modifiers in Python. So there's no 
-# explicit way of creating private members or anything like that. 
+# explicit way of creating private members or anything like that.
+#Also, python child classes don't call the constructor or the destructor
+# of the parent classes automatically. 
 
 main();
