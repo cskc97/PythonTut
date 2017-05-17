3b16579067b00cb3573bda40961291f773ae5fed
Santhoshkrishnachaitanya Chelikavada <everythingskc@gmail.com>
2017-05-17 16:00:03 +0530

    stuff

--- tuts/classes.py
+++ tuts/classes.py
@@ -8,10 +8,17 @@ class Animal:
 
 
 class Duck(Animal): #Duck inherits from Animal
-	def __init__(self,duckName):
+	def __init__(self,duckName,**kwargs):
 		self.name = duckName
 		super(Duck, self).__init__() #constructor needs to be explicitly
 									# if required.
+		self.kwargDict = kwargs
+
+	def printKwargs(self):
+		for key in self.kwargDict:
+			print(key,self.kwargDict.get(key))
+
+
 
 	def quack(self):
 		print("Quack!")
@@ -22,14 +29,25 @@ class Duck(Animal): #Duck inherits from Animal
 
 
 def main():
-	donald = Duck("Donald The Duck")
+	donald = Duck("Donald The Duck",color="Red")
+
 	print("Created {}".format(donald.name))
 	donald.quack()
 	donald.walk()
+	donald.printKwargs()
+
+	# dictionary = dict(color = "Red",num = 100)
+	# print(dictionary["color"])
+	# dictionary = {"color":"blue"}
+	# print(dictionary["color"])
 
 #NOTE: There are NO access modifiers in Python. So there's no 
 # explicit way of creating private members or anything like that.
 #Also, python child classes don't call the constructor or the destructor
-# of the parent classes automatically. 
+# of the parent classes automatically.
+# What this means is that accessor methods aren't really required.
+# But it's still a good idea to have get and set methods to access member variables
+# because you can make additional modifications to the properties of the object if required.
+
 
 main();
