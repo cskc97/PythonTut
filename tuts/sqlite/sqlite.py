c2cc09061582d8b200f59558af68e5a363a94385
Santhoshkrishnachaitanya Chelikavada <everythingskc@gmail.com>
2017-05-22 17:37:49 +0530

    stuff

--- tuts/sqlite/sqlite.py
+++ tuts/sqlite/sqlite.py
@@ -2,9 +2,14 @@ import sqlite3
 
 def main():
     db = sqlite3.connect("test.db");
+    #Adding db.row_factory = sqlite3.Row will return row objects instead of
+    #values in the row. And you can use it as a dictionary if required also
+
+
     db.execute("DROP TABLE IF EXISTS Students")
     db.execute("CREATE TABLE Students(name TEXT, age INT)")
     db.execute("INSERT INTO Students VALUES (?,?)",("Santhosh",19))
+    db.execute("INSERT INTO Students VALUES (\'Another Santhosh\',19)")
 
     db.commit()
 
