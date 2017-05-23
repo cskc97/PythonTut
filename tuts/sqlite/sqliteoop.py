29f2e03272b3198a60dab829483abe5fac7ce204
Santhoshkrishnachaitanya Chelikavada <everythingskc@gmail.com>
2017-05-23 17:33:43 +0530

    stuff

--- tuts/sqlite/sqliteoop.py
+++ tuts/sqlite/sqliteoop.py
@@ -10,12 +10,31 @@ class DatabaseManager:
         self._db.commit()
 
     def insertInDB(self,studentName,studentAge):
-        
+        self._db.execute("INSERT INTO {} VALUES(?,?)".format(self._dbName),(studentName,studentAge))
+
+    def allRows(self):
+        cursor = self._db.execute("SELECT * FROM {}".format(self._dbName))
+        for row in cursor:
+            print(row)
+
+    def getTupleByName(self,studentName):
+
+            cursor = self._db.execute("SELECT * FROM {} WHERE name=?".format(self._dbName),(studentName,))
+            print(cursor.fetchone())
+
+
+
+
+
+
 
 
 
 
 dbManager = DatabaseManager(dbname="dbmanager",tablename="students");
+dbManager.insertInDB("Santhosh",19)
+dbManager.allRows()
+dbManager.getTupleByName("Santhosh")
 
 
 
