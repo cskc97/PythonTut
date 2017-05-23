a27aba52bbfcb8b1c6923afde907badce1398787
Santhoshkrishnachaitanya Chelikavada <everythingskc@gmail.com>
2017-05-23 17:23:45 +0530

    stuff

--- tuts/sqlite/sqliteoop.py
+++ tuts/sqlite/sqliteoop.py
@@ -1,10 +1,22 @@
 import sqlite3
 
 class DatabaseManager:
-    def __int__(self,**kwargs):
+    def __init__(self,**kwargs):
         self._dbName = kwargs["dbname"]
         self._db = sqlite3.connect(self._dbName);
-        self._tableName = kwargs.get("tablename","unknown.db");
+        self._tableName = kwargs.get("tablename","unknown");
+        self._db.execute("DROP TABLE IF EXISTS {}".format(self._dbName))
+        self._db.execute("CREATE TABLE {}(name TEXT, age INT)".format(self._dbName));
+        self._db.commit()
 
-    
+    def insertInDB(self,studentName,studentAge):
+        
+
+
+
+
+dbManager = DatabaseManager(dbname="dbmanager",tablename="students");
+
+
+
 
