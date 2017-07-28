f31ec84d875648ea81b69fba055ab291077636c6
Santhoshkrishnachaitanya Chelikavada <cskc97@users.noreply.github.com>
2017-07-28 15:38:56 -0400

    Update testaclassasyncio.py

--- tuts/mocking/testaclassasyncio.py
+++ tuts/mocking/testaclassasyncio.py
@@ -1,13 +1,8 @@
 import unittest
 from unittest.mock import patch
-from unittest.mock import Mock
 from asyncio import *
-
 from aiohttp.test_utils import unittest_run_loop
-
 from aclass import AClass
-
-
 
 
 class AClassTest(unittest.TestCase):
@@ -18,23 +13,16 @@ class AClassTest(unittest.TestCase):
 
     @unittest_run_loop
     @patch("aclass.AClass.return_sum")
-    async def test_subtract(self,mock_sum):
+    async def test_subtract(self, mock_sum):
 
         a_obj = AClass()
-        mock_sum.return_value=5
+        mock_sum.return_value = 5
         print(mock_sum)
         print(mock_sum.return_value)
 
-        ret_val =  await a_obj.subtract(10,5)
+        ret_val = await a_obj.subtract(10, 5)
 
-        self.assertEqual(ret_val,mock_sum.return_value)
+        self.assertEqual(ret_val, mock_sum.return_value)
 
-
-
-if __name__=="__main__":
+if __name__ == "__main__":
     unittest.main()
-
-
-
-
-
